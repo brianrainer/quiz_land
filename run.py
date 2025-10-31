a9b118d691b8557c6192fbc0ea3fc4d5f8cd6dc8
Brian Rainer <brianbulletworks@gmail.com>
2025-10-31 11:40:09 +0700

    Add header and layout for registration, signin, and dashboard

--- run.py
+++ run.py
@@ -17,5 +17,17 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
